19c7eae8bd4c9607bf18faa1f53fb2261425d26f
Thomas Thrainer <thomas.thrainer@jambit.com>
2012-12-20 11:43:10 +0100

    Add sbteclipse plugin and change deprecated class

--- src/main/scala/SassPlugin.scala
+++ src/main/scala/SassPlugin.scala
@@ -7,7 +7,7 @@ import play.Project._
 object SassPlugin extends Plugin {
     val sassEntryPoints = SettingKey[PathFinder]("play-sass-entry-points")
     val sassOptions = SettingKey[Seq[String]]("play-sass-options")
-    val sassWatcher = PlayProject.AssetsCompiler("sass",
+    val sassWatcher = AssetsCompiler("sass",
         { file => (file ** "*.sass") +++ (file ** "*.scss") },
         sassEntryPoints,
         { (name, min) => 
